f2eacc54ed74b7c0c1a8dd4309fe381437655a30
Fahimahmed71 <88280947+Fahimahmed71@users.noreply.github.com>
2022-10-14 23:55:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import CheckOut from "./components/CheckOut/CheckOut";
+import About from "./components/Home/About/About";
+import AddServices from "./components/Home/AddServices/AddServices";
+import Home from "./components/Home/Home";
+import ServicesDetails from "./components/Home/Services/Service/ServicesDetails/ServicesDetails";
+import ManageServices from "./components/ManageServices/ManageServices";
+import Order from "./components/Order/Order";
+import Login from "./components/Register/Login/Login";
+import RequiredAuth from "./components/Register/RequiredAuth/RequiredAuth";
+import SignUp from "./components/Register/SignUp/SignUp";
+import Footer from "./components/Shared/Footer/Footer";
+import NavBar from "./components/Shared/Nav/Nav";
+import NotFound from "./components/Shared/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <Footer></Footer>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/service/:serviceId" element={<ServicesDetails />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route
+          path="/checkout"
+          element={
+            <RequiredAuth>
+              <CheckOut />
+            </RequiredAuth>
+          }
+        ></Route>
+        <Route
+          path="/add"
+          element={
+            <RequiredAuth>
+              <AddServices />
+            </RequiredAuth>
+          }
+        ></Route>
+        <Route
+          path="/manage"
+          element={
+            <RequiredAuth>
+              <ManageServices />
+            </RequiredAuth>
+          }
+        ></Route>
+        <Route
+          path="/order"
+          element={
+            <RequiredAuth>
+              <Order />
+            </RequiredAuth>
+          }
+        ></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
